fbd188039d375d53e91b5a504c48439cf8193d95
bang-koon <dol7966@gmail.com>
2025-12-01 16:56:50 +0900

    3.interactive circle 주석 추가

--- 3. interactive circles with mouse movement/canvas.js
+++ 3. interactive circles with mouse movement/canvas.js
@@ -20,10 +20,10 @@ addEventListener("resize", () => {
 });
 
 class Circle {
-  radius = Math.random() * 4 + 1;
+  radius = Math.random() * 4 + 1; // 1은 최솟값
   x = Math.random() * (innerWidth - this.radius * 2) + this.radius;
   y = Math.random() * (innerHeight - this.radius * 2) + this.radius;
-  dx = (Math.random() - 0.5) * 8;
+  dx = (Math.random() - 0.5) * 8; // 움직임의 방향을 좌우로 설정 * 속도
   dy = (Math.random() - 0.5) * 8;
   min_radius = this.radius;
 
@@ -35,16 +35,18 @@ class Circle {
   }
 
   update() {
+    // 화면 끝으로 가면 방향 바꾸기
     if (this.x + this.radius >= innerWidth || this.x - this.radius <= 0) {
       this.dx = -this.dx;
     }
     if (this.y + this.radius >= innerHeight || this.y - this.radius <= 0) {
       this.dy = -this.dy;
     }
+    // dx값만큼 x 옮기기
     this.x += this.dx;
     this.y += this.dy;
 
-    // interactivity
+    // 마우스 근처이고 MAX_RADIUS보다 radius가 작다면 크기 키우기, 근처가 아니고 min_radius보다 크다면 줄이기
     if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
       if (this.radius < MAX_RADIUS) this.radius += 1;
     } else if (this.radius > this.min_radius) this.radius -= 1;
